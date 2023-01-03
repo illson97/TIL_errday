5363bca835b0b736c177cc2ec2b52f8e0ce9b2ee
illson97 <121412131+illson97@users.noreply.github.com>
2023-01-03 17:45:51 +0900

    Add files via upload

---
+++ TIL_7th/python_re.py
@@ -0,0 +1,84 @@
+
+
+#a = -6
+#if a > 0:
+    #print('True')
+#else:
+    #print('False')
+
+
+
+#a = 6
+#b = 5
+
+#if a > b:
+    #print(a)
+#elif a < b:
+    #print(b)
+#else: 
+    #print('False')
+
+
+
+#a = 10
+
+#if a>1 and a<10:
+    #print('True')
+#else:
+    #print('False')
+
+
+
+#a = 7
+#if a % 2 == 0 and a>0:
+    #print('True')
+#else:
+    #print('False')
+
+
+#a = 59
+
+#if a>100 or a<0:
+    #print('ERROR')
+#elif a>=60:
+    #print('합격')
+#elif a<60:
+    #print('불합격')
+
+
+#string = "sushi"
+#for element in string[::-1]:
+    #print(element)
+
+
+#a = 9
+#b = 18
+
+#for element in range(min(a,b)+1, max(a,b)):
+    #print(element)
+#if a == b:
+    #print("False")
+
+
+#a = 9
+#b = 18
+
+#for element in range(min(a,b)+1, max(a,b))[::-1]:
+    #print(element, end=" ")
+#if a == b:
+    #print("False")
+
+
+#a = 1
+
+#for element in range(1, a + 1, 2):
+    #print(element)
+#if a<1:
+    #print("False")
+
+
+#for element in range(2, 10):
+    #for i in range(1, 10):
+        #print(element * i)
+    #print('')
+
