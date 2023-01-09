0896c29b325f4038cf1a778b825abd24812c136c
illson97 <121412131+illson97@users.noreply.github.com>
2023-01-09 21:35:40 +0900

    Add files via upload

---
+++ TIL_11th/in,output_practice.py
@@ -0,0 +1,84 @@
+# 1번
+
+#number = int(input())
+#print(number)
+
+
+
+
+
+# 2번
+#a, b = map(int, input().split())
+#print(a, b)
+
+
+
+
+
+# 3번
+#nums = list(map(int, input().split()))
+
+#for i in nums:
+    #print(i, end='')
+
+
+
+
+# 4번
+#string = input().split()
+
+#for i in string:
+    #print(i, end='')
+
+
+
+
+# 5번
+#nums = list(map(int, input().split()))
+
+#for i in nums:
+    #print(i, end='')
+
+
+
+
+# 6번
+#n = int(input())
+
+#print(-n, n)
+
+
+
+# 7번
+#string = input().split()
+
+#for i in string:
+    #print(i, end='')
+
+
+
+
+# 8번 랜덤
+#import random
+#num = []
+#while len(num) < 10:
+    #i = random.randint(1,99)
+    #if i not in num:
+        #num.append(i)
+
+#print(sorted(num))
+
+
+
+
+# 8번
+#n = list(input().split())
+
+#print(''.join(n))
+
+
+
+nums = list(map(int, input().split()))
+
+for i in nums:
+    print(i, end='')
