3f8e76e1c528cd0532bc872f5fbac06c9e9e0909
illson97 <121412131+illson97@users.noreply.github.com>
2023-01-11 22:47:09 +0900

    Add files via upload

---
+++ TIL_12th/12th.py
@@ -0,0 +1,76 @@
+
+
+#def add(a, b):
+    #return a + b
+
+#print(add(-10, -20))
+#print(add(10, 20))
+
+
+#def cube(n):
+    #return n**3
+
+#print(cube(2))
+#print(cube(100))
+
+
+
+# 문제 1
+#num = int(input())
+
+#print(num)
+
+
+# 문제 2
+
+#string = input().split()
+
+#print(string)
+
+# 문제 3
+
+#T = int(input())
+
+#for t in range(1, T+1):
+    #print(t)
+    #pass
+
+# 문제 4
+
+#num = list(map(int, input().split()))
+
+#print(num)
+
+# 문제 5
+
+#a, b = list(map(int, input().split()))
+
+#print(a, b)
+
+
+
+# 문제 6
+
+#string = input().split()
+
+#print(string)
+
+
+
+# 문제 7
+
+#T = int(input())
+
+#for t in range(1, T+1):
+    #num = list(map(int, input().split()))
+    #print(num)
+
+
+
+# 문제 8
+
+#T = int(input())
+
+#for t in range(1, T+1):
+    #num = list(map(int, input().split()))
+    #print(num)
---
+++ TIL_13th/13th.py
@@ -0,0 +1,71 @@
+
+# 1번
+# n = list(map(int, input().split()))
+
+# print(n)
+
+
+
+# 2번
+#string = input().split()
+
+#print(*string)
+
+
+
+
+# 3번
+#T = int(input())
+
+#for t in range(1, T+1):
+    #N = int(input())
+    #for _ in range(1, N+1):
+        #print(_)
+        #pass
+
+
+
+# 4번
+#T = int(input())
+
+#for t in range(1, T+1):
+    #N = int(input())
+    #for _ in range(1, N+1):
+        #print(_, t)
+        #pass
+
+
+
+
+
+# 5번
+#T = int(input())
+
+#for t in range(1, T+1):
+    #N = int(input())
+    #for _ in range(1, N+1):
+        #string = list(map(int, input().split()))
+        #print(*string)
+        #pass
+
+
+
+
+# 6번
+
+
+
+
+
+
+
+# 7, 8, 9번
+#n1, n2 = list(map(int, input().split()))
+#for n1 in range(1, n1 + 1):
+    #for n2 in range(1, n2 + 1):
+        #nums = list(map(int, input().split()))
+        #print(nums)
+
+
+
+
