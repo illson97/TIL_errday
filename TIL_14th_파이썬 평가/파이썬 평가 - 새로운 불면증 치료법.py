952cd6777d9c3207e125acf18bfb518e848bcac4
illson97 <121412131+illson97@users.noreply.github.com>
2023-01-13 21:11:30 +0900

    Add files via upload

---
+++ TIL_14th_파이썬 평가/파이썬 평가 - 새로운 불면증 치료법.py
@@ -0,0 +1,19 @@
+
+
+# 새로운 불면증 치료법
+
+
+# T = int(input())
+
+# for test_case in range(1, T+1):
+    # N = int(input())
+    # nums = [0] * 10
+    
+    # i = 1
+    # while 0 in nums:
+        # num = str(N * i)
+        # for j in range(len(num)):
+            # nums[int(num[j])] += 1
+        # i += 1
+
+    # print("#" + str(test_case) + " " + num)        
---
+++ TIL_14th_파이썬 평가/파이썬 평가 - 서랍의 비밀번호.py
@@ -0,0 +1,7 @@
+
+
+# 서랍의 비밀번호
+
+# a, b = map(int, input().split())
+# answer = a - b + 1
+# print(answer)
