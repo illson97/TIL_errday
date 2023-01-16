228bcd70730aa238f13dcfcb9dff713fe9438546
illson97 <121412131+illson97@users.noreply.github.com>
2023-01-16 17:51:27 +0900

    Add files via upload

---
+++ TIL_16th/16th.py
@@ -0,0 +1,58 @@
+
+# 1000
+
+#a, b = map(int, input().split())
+
+#print(a + b)
+
+
+# 2558
+
+#A = int(input())
+#B = int(input())
+
+#print(A + B)
+
+
+# 10950
+
+#T = int(input())
+
+#for t in range(T):
+    #a, b = map(int, input().split())
+    
+    #print(a + b)
+
+
+
+
+# 10953
+
+#T = int(input())
+
+#for t in range(T):
+    #a, b = map(int, input().split(','))
+    
+    #print(a + b)
+
+
+
+
+# 11021
+
+#T = int(input())
+
+#for t in range(1, T+1):
+    #a, b = map(int, input().split())
+    
+    #print("Case #"+ str(t) + ":", a + b)
+
+
+
+# 11022
+
+#T= int(input())
+
+#for i in range(1, T+1):
+    #a, b = map(int, input().split())
+    #print(f'Case #{i}: {a} + {b} = {a +b}')
