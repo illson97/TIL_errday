7d4f83e20e6bad7b77c9e2721f1a52b2fb9e79f6
illson97 <illson97@github.com>
2023-03-24 15:18:44 +0900

    TIL_django05_exercise

---
+++ TIL_django/TIL_django05_exercise/fivthexercise/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fivthexercise project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fivthexercise.settings')
+
+application = get_wsgi_application()
